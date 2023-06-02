3e4ce3c60ed5da5994aacf3eab16215b363fd76e
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2023-06-02 08:10:56 +0000

    Added http auth guard

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,17 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
+        // Configure the Http-Guard library to use cache
+        // Uncomment the code below to enable caching in authentication guard
+        // \Drewlabs\HttpGuard\HttpGuardGlobals::usesCache(true);
+
+        // Configure the http-guard library to use PHP 'memcached' storage as default driver
+        // Uncomment the code below to enable using memcached driver as authentication guard caching
+        // \Drewlabs\HttpGuard\HttpGuardGlobals::useCacheDriver('memcached');
+
+        \Drewlabs\HttpGuard\HttpGuardGlobals::userPath('auth/v2/user');
+        \Drewlabs\HttpGuard\HttpGuardGlobals::revokePath('auth/v2/logout');
+        // ...
         //
     }
 }
