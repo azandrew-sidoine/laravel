fb1c9bd54f8deaacd8c51fac0b1f6915192c956a
azandrew-sidoine <azandrewdevelopper@gmail.com>
2024-03-21 22:18:44 +0000

    Removed docker related configurations

--- docker/cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env /bin/sh
-
-cleanup_app_directory() {
-    if [ -d "$APP_DIR/docker" ]; then
-        rm -rf "$APP_DIR/docker" "$APP_DIR/.Dockerfile" "$APP_DIR/.dockerignore" "$APP_DIR/.vite.config.js"
-    fi
-}
-cleanup_app_directory
